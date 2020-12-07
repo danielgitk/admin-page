e54bf6081ef372ccb0898680978a1316eb906957
Daniel <daniel@192.168.1.8>
2020-12-07 04:08:37 +0300

    almost there final for the night

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  [x: string]: any;
 
   @Output() public sidenavToggle = new EventEmitter();
   constructor() { }
@@ -16,4 +17,10 @@ export class HeaderComponent implements OnInit {
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
   }
+  logout() {
+    localStorage.removeItem("token");
+
+    this.router.navigate(["login"]);
+  }
+
 }
